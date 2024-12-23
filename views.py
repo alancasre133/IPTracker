417f440df1a6e03a51317dbd691aaa9b1cef94b9
Alan Axel Castro Resendiz <72096324+alancasre133@users.noreply.github.com>
2024-12-22 19:13:31 -0600

    Update views.py

--- views.py
+++ views.py
@@ -11,14 +11,5 @@ def home():
 
     ip_address = ""
     more = ""
-    forwarded_for = request.headers.get('X-Forwarded-For')
-    forwarded_for="189.217.111.179"
-    if forwarded_for:
-        for ip in forwarded_for.split(','):
-            ip_address+=ip+" , "
-            more += "1"
-            approximately_house = requests.get(f'https://ipinfo.io/{ip}?token=9c06a926e98489').json()
-            IP,city,region,country, postal, loc = getIPData(approximately_house)
-            approximately_house = f'\n IP:{IP}\nCity:{city}\nRegion:{region}\nCountry:{country}\nPostal Code:{postal}\nloc:{loc}'
-            insertDocuments(collection,[{"IP":IP,"loc": loc, "country": country, "region": region, "city": city, "postal":postal}])
+
     return render_template("index.html",name="Alan")
