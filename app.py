dac491823aaea7040eb88d18a99834c5d8307703
Alan Axel Castro Resendiz <72096324+alancasre133@users.noreply.github.com>
2024-12-22 19:20:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from pymongo.server_api import ServerApi
 from urllib.parse import quote_plus
 
 app = Flask(__name__)
-#app.register_blueprint(views,url_prefix="/")
+app.register_blueprint(views,url_prefix="/")
 
 if __name__ == '__main__':
     app.run(debug=True)
