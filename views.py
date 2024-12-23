434f7f5a6d5cd30db83cc5848bd9a9be6769403e
Alan Axel Castro Resendiz <72096324+alancasre133@users.noreply.github.com>
2024-12-22 19:14:38 -0600

    Update views.py

--- views.py
+++ views.py
@@ -7,9 +7,5 @@ views = Blueprint(__name__,"views")
 @views.route("/")
 def home():
     approximately_house = ""
-    collection = getCollection(getClient("alanaxelcastroresendiz","Alan7776"),'RACPIR','IP_Metadata')
-
-    ip_address = ""
-    more = ""
 
     return render_template("index.html",name="Alan")
